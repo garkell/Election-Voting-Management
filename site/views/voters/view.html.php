3a75c6bd3f0b65a506f602073f41a48a24ff9b68
Glenn Arkell <glennarkell@gmail.com>
2020-09-09 18:03:23 +1000

    Add files via upload

---
+++ site/views/voters/view.html.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @version    4.0.01
+ * @package    Com_Gavoting
+ * @author     Glenn Arkell <glenn@glennarkell.com.au>
+ * @copyright  2020 Glenn Arkell
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// No direct access
+defined('_JEXEC') or die;
+
+use \Joomla\CMS\Factory;
+use \Joomla\CMS\Language\Text;
+use \Joomla\CMS\MVC\View\GenericDataException;
+use \Joomla\CMS\MVC\View\HtmlView as BaseHtmlView;
+
+/**
+ * View class for a list of Gavoting.
+ * @since  1.6
+ */
+class GavotingViewVoters extends BaseHtmlView
+{
+	protected $items;
+
+	protected $pagination;
+
+	protected $state;
+
+	protected $params;
+
+	/**
+	 * Display the view
+	 *
+	 * @param   string  $tpl  Template name
+	 *
+	 * @return void
+	 *
+	 * @throws Exception
+	 */
+	public function display($tpl = null)
+	{
+		$app = Factory::getApplication();
+
+		$this->state = $this->get('State');
+		$this->items = $this->get('Items');
+		$this->pagination = $this->get('Pagination');
+		$this->params = $app->getParams('com_gavoting');
+		$this->filterForm = $this->get('FilterForm');
+		$this->activeFilters = $this->get('ActiveFilters');
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors'))) {
+			throw new GenericDataException(implode("\n", $errors), 500);
+		}
+
+		return parent::display($tpl);
+	}
+
+	/**
+	 * Check if state is set
+	 * @param   mixed  $state  State
+	 * @return bool
+	 */
+	public function getState($state)
+	{
+		return isset($this->state->{$state}) ? $this->state->{$state} : false;
+	}
+}
