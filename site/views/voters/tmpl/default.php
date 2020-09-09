4a3d6c2f1944ba486717b648fd08d3ce6cfddc1c
Glenn Arkell <glennarkell@gmail.com>
2020-09-09 18:02:57 +1000

    Add files via upload

--- site/views/voters/tmpl/default.php
+++ site/views/voters/tmpl/default.php
@@ -1,1 +1,173 @@
+<?php
+/**
+ * @version    4.0.01
+ * @package    Com_Gavoting
+ * @author     Glenn Arkell <glenn@glennarkell.com.au>
+ * @copyright  2020 Glenn Arkell
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// No direct access
+defined('_JEXEC') or die;
 
+use \Joomla\CMS\HTML\HTMLHelper;
+use \Joomla\CMS\Factory;
+use \Joomla\CMS\Uri\Uri;
+use \Joomla\CMS\Router\Route;
+use \Joomla\CMS\Language\Text;
+use \Joomla\CMS\Layout\LayoutHelper;
+
+HTMLHelper::_('behavior.core');
+HTMLHelper::_('behavior.formvalidator');
+HTMLHelper::_('behavior.keepalive');
+
+// Load admin language file
+$lang = Factory::getLanguage();
+$lang->load('com_gavoting', JPATH_ADMINISTRATOR);
+
+$wa = Factory::getApplication()->getDocument()->getWebAssetManager();
+$wa->registerAndUseStyle('gavotingCore', 'com_gavoting/gavoting.css');
+$wa->registerAndUseStyle('gavotingList', 'com_gavoting/list.css');
+
+$user       = Factory::getUser();
+$userId     = $user->get('id');
+$listOrder  = $this->state->get('list.ordering');
+$listDirn   = $this->state->get('list.direction');
+$canCreate  = $user->authorise('core.create', 'com_gavoting');
+$canEdit    = $user->authorise('core.edit', 'com_gavoting');
+$canCheckin = $user->authorise('core.manage', 'com_gavoting');
+$canChange  = $user->authorise('core.edit.state', 'com_gavoting');
+$canDelete  = $user->authorise('core.delete', 'com_gavoting');
+
+?>
+
+<h2><?php echo Text::_('COM_GAVOTING_TITLE_VOTERS'); ?></h2>
+
+<form action="<?php echo htmlspecialchars(Uri::getInstance()->toString()); ?>" method="post"
+      name="adminForm" id="adminForm">
+
+	<?php echo LayoutHelper::render('default_filter', array('view' => $this), dirname(__FILE__)); ?>
+
+    <div class="com-contact-categories categories-list">
+	<table class="table table-striped" id="voterList">
+		<thead>
+		<tr>
+			<?php if (isset($this->items[0]->state)): ?>
+				<th width="5%">
+					<?php echo HTMLHelper::_('grid.sort', 'JSTATUS', 'a.state', $listDirn, $listOrder); ?>
+				</th>
+			<?php endif; ?>
+
+			<th class=''>
+				<?php echo HTMLHelper::_('grid.sort',  'COM_GAVOTING_VOTERS_USER_ID', 'v.name', $listDirn, $listOrder); ?>
+			</th>
+			<th class='center'>
+				<?php echo HTMLHelper::_('grid.sort',  'COM_GAVOTING_VOTERS_CAT_ID', 'cat_id_name', $listDirn, $listOrder); ?>
+			</th>
+			<th class='center'>
+				<?php echo HTMLHelper::_('grid.sort',  'COM_GAVOTING_VOTERS_PROXY_VOTE', 'a.proxy_vote', $listDirn, $listOrder); ?>
+			</th>
+
+			<?php if ($canEdit || $canDelete): ?>
+				<th width="15%" class="center"><?php echo Text::_('COM_GAVOTING_ACTIONS'); ?></th>
+			<?php endif; ?>
+
+		</tr>
+		</thead>
+		<tfoot>
+		<tr>
+			<td colspan="<?php echo isset($this->items[0]) ? count(get_object_vars($this->items[0])) : 10; ?>">
+				<?php echo $this->pagination->getListFooter(); ?>
+			</td>
+		</tr>
+		</tfoot>
+		<tbody>
+		<?php foreach ($this->items as $i => $item) : ?>
+			<?php $canEdit = $user->authorise('core.edit', 'com_gavoting'); ?>
+
+			<?php if (!$canEdit && $user->authorise('core.edit.own', 'com_gavoting')): ?>
+				<?php $canEdit = Factory::getUser()->id == $item->created_by; ?>
+			<?php endif; ?>
+
+			<tr class="row<?php echo $i % 2; ?>">
+
+				<?php if (isset($this->items[0]->state)) : ?>
+					<?php $class = ($canChange) ? 'active' : 'disabled'; ?>
+					<td class="center">
+						<?php if ($item->state == -2): ?>
+						<a class="btn btn-micro <?php echo $class; ?>" href="<?php echo ($canChange) ? Route::_('index.php?option=com_gavoting&task=voter.publish&id=' . $item->id . '&state=' . ((0) % 2), false, 2) : '#'; ?>">
+						<?php else : ?>
+						<a class="btn btn-micro <?php echo $class; ?>" href="<?php echo ($canChange) ? Route::_('index.php?option=com_gavoting&task=voter.publish&id=' . $item->id . '&state=' . (($item->state + 1) % 2), false, 2) : '#'; ?>">
+						<?php endif; ?>
+						<?php if ($item->state == 1): ?>
+							<i class="icon-publish" title="Active"></i>
+						<?php elseif ($item->state == 0): ?>
+							<i class="icon-unpublish" title="Not Active"></i>
+						<?php elseif ($item->state == -2): ?>
+							<i class="icon-trash" title="Trashed"></i>
+						<?php elseif ($item->state == 2): ?>
+							<i class="icon-archive" title="Archived"></i>
+						<?php endif; ?>
+						</a>
+					</td>
+				<?php endif; ?>
+
+				<td>
+					<?php if (isset($item->checked_out) && $item->checked_out) : ?>
+						<?php echo HTMLHelper::_('jgrid.checkedout', $i, $item->uEditor, $item->checked_out_time, 'voters.', $canCheckin); ?>
+					<?php endif; ?>
+					<a style="color:#3c4d91;" href="<?php echo Route::_('index.php?option=com_gavoting&view=voter&id='.(int) $item->id); ?>">
+						<?php echo $this->escape($item->user_id_name); ?>
+					</a>
+				</td>
+				<td class="center">
+					<?php echo $item->cat_id_name; ?>
+				</td>
+				<td class="center">
+					<?php if ($item->proxy_vote) { echo 'Yes ('.$item->created_by_name.')'; } else { echo 'No'; } ?>
+				</td>
+
+				<?php if ($canEdit || $canDelete): ?>
+					<td class="center">
+						<?php if ($canEdit): ?>
+							<a href="<?php echo Route::_('index.php?option=com_gavoting&task=voterform.edit&id=' . $item->id, false, 2); ?>" class="btn-secondary btn-mini" type="button" title="Edit Record"><i class="icon-edit" ></i></a> &nbsp; &nbsp;
+						<?php endif; ?>
+						<?php if ($canDelete): ?>
+							<a href="<?php echo Route::_('index.php?option=com_gavoting&task=voterform.remove&id=' . $item->id, false, 2); ?>" class="btn-secondary btn-mini delete-button" type="button" title="Delete Record"><i class="icon-trash" ></i></a> &nbsp; &nbsp;
+							<a href="<?php echo Route::_('index.php?option=com_gavoting&task=voterform.archive&id=' . $item->id, false, 2); ?>" class="btn-secondary btn-mini" type="button" title="Archive Record"><i class="icon-archive" ></i></a>
+						<?php endif; ?>
+					</td>
+				<?php endif; ?>
+
+			</tr>
+		<?php endforeach; ?>
+		</tbody>
+	</table>
+    </div>
+	<?php if ($canCreate) : ?>
+		<a href="<?php echo Route::_('index.php?option=com_gavoting&task=voterform.edit&id=0', false, 0); ?>"
+		   class="btn btn-success btn-small"><i class="icon-plus"></i> <?php echo Text::_('COM_GAVOTING_ADD_ITEM'); ?>
+		</a>
+	<?php endif; ?>
+
+	<input type="hidden" name="task" value=""/>
+	<input type="hidden" name="boxchecked" value="0"/>
+	<input type="hidden" name="filter_order" value="<?php echo $listOrder; ?>"/>
+	<input type="hidden" name="filter_order_Dir" value="<?php echo $listDirn; ?>"/>
+	<?php echo HTMLHelper::_('form.token'); ?>
+</form>
+
+<?php if($canDelete) : ?>
+<script type="text/javascript">
+
+	jQuery(document).ready(function () {
+		jQuery('.delete-button').click(deleteItem);
+	});
+
+	function deleteItem() {
+
+		if (!confirm("<?php echo Text::_('COM_GAVOTING_DELETE_MESSAGE'); ?>")) {
+			return false;
+		}
+	}
+</script>
+<?php endif; ?>
---
+++ site/views/voters/tmpl/default_filter.php
@@ -0,0 +1,135 @@
+<?php
+
+/**
+ * @version    4.0.01
+ * @package    Com_Gavoting
+ * @author     Glenn Arkell <glenn@glennarkell.com.au>
+ * @copyright  2020 Glenn Arkell
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('JPATH_BASE') or die;
+
+use \Joomla\CMS\HTML\HTMLHelper;
+use \Joomla\CMS\Factory;
+use \Joomla\CMS\Language\Text;
+
+$data = $displayData;
+
+// Receive overridable options
+$data['options'] = !empty($data['options']) ? $data['options'] : array();
+
+// insert this to have pagination selection available
+$pages = $data['view']->get('pagination');
+
+// Check if any filter field has been filled
+$filters       = false;
+$filtered      = false;
+$search_filter = false;
+$show_filter = false;
+
+if (isset($data['view']->filterForm))
+{
+	$filters = $data['view']->filterForm->getGroup('filter');
+}
+
+// Check if there are filters set.
+if ($filters !== false)
+{
+	$filterFields = array_keys($filters);
+	$filled       = false;
+
+	foreach ($filterFields as $filterField)
+	{
+		$filterField = substr($filterField, 7);
+		$filter      = $data['view']->getState('filter.' . $filterField);
+
+		if (!empty($filter))
+		{
+			$filled = $filter;
+		}
+
+		if (!empty($filled))
+		{
+			$filtered = true;
+			break;
+		}
+	}
+
+	$search_filter = $filters['filter_search'];
+	unset($filters['filter_search']);
+}
+
+$options = $data['options'];
+
+// Set some basic options
+$customOptions = array(
+	'filtersHidden'       => isset($options['filtersHidden']) ? $options['filtersHidden'] : empty($data['view']->activeFilters) && !$filtered,
+	'defaultLimit'        => isset($options['defaultLimit']) ? $options['defaultLimit'] : Factory::getApplication()->get('list_limit', 20),
+	'searchFieldSelector' => '#filter_search',
+	'orderFieldSelector'  => '#list_fullordering'
+);
+
+$data['options'] = array_unique(array_merge($customOptions, $data['options']));
+
+$formSelector = !empty($data['options']['formSelector']) ? $data['options']['formSelector'] : '#adminForm';
+
+// Load search tools
+HTMLHelper::_('searchtools.form', $formSelector, $data['options']);
+?>
+
+<div class="js-stools" style="margin-bottom: 20px;">
+	<div class="clearfix">
+		<div class="js-stools-container-bar" style="width:100%;">
+			<label for="filter_search" class="element-invisible float-left"
+				aria-invalid="false"><?php echo Text::_('COM_GAVOTING_SEARCH_FILTER_SUBMIT'); ?></label>
+
+			<div class="btn-wrapper form-inline float-left">
+				<?php echo $search_filter->input; ?>
+				<button type="submit" class="btn btn-primary hasTooltip float-left" title=""
+					data-original-title="<?php echo Text::_('COM_GAVOTING_SEARCH_FILTER_SUBMIT'); ?>">
+					<i class="icon-search"></i>
+				</button>
+			</div>
+			<?php if ($show_filter): ?>
+				<?php if ($filters): ?>
+					<div class="btn-wrapper float-left hidden-phone" style="margin-left: 5px;">
+						<button type="button" class="btn btn-info hasTooltip js-stools-btn-filter" title=""
+							data-original-title="<?php echo Text::_('COM_GAVOTING_SEARCH_TOOLS_DESC'); ?>">
+							<?php echo Text::_('COM_GAVOTING_SEARCH_TOOLS'); ?> <i class="caret"></i>
+						</button>
+					</div>
+				<?php endif; ?>
+			<?php endif; ?>
+
+			<div class="btn-wrapper float-left" style="margin-left: 5px;">
+				<button type="button" class="btn btn-secondary hasTooltip js-stools-btn-clear" title=""
+					data-original-title="<?php echo Text::_('COM_GAVOTING_SEARCH_FILTER_CLEAR'); ?>"
+					onclick="jQuery(this).closest('form').find('input').val('');submit();">
+					<?php echo Text::_('COM_GAVOTING_SEARCH_FILTER_CLEAR'); ?>
+				</button>
+			</div>
+
+			<?php // insert this to have pagination selection available   ?>
+			<div class="btn-group float-right hidden-phone">
+				<label for="limit" class="element-invisible"><?php echo Text::_('JFIELD_PLG_SEARCH_SEARCHLIMIT_DESC');?></label>
+				<?php echo $pages->getLimitBox(); ?>
+			</div>
+		</div>
+	</div>
+	<!-- Filters div -->
+	<?php if ($show_filter): ?>
+		<div class="js-stools-container-filters hidden-phone clearfix" style="">
+			<?php // Load the form filters ?>
+			<?php if ($filters) : ?>
+				<?php foreach ($filters as $fieldName => $field) : ?>
+					<?php if ($fieldName != 'filter_search') : ?>
+						<div class="js-stools-field-filter">
+							<?php echo $field->renderField(array('hiddenLabel' => false)); ?>
+						</div>
+					<?php endif; ?>
+				<?php endforeach; ?>
+			<?php endif; ?>
+		</div>
+	<?php endif; ?>
+</div>
