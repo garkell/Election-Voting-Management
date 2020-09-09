27fe0c198bd79a163b0d633f26806486b71dec1d
Glenn Arkell <glennarkell@gmail.com>
2020-09-09 18:26:20 +1000

    Add files via upload

---
+++ site/views/positionform/view.html.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @version    4.0.01
+ * @package    Com_Gavoting
+ * @author     Glenn Arkell <glenn@glennarkell.com.au>
+ * @copyright  2020 Glenn Arkell
+ * @license    GNU General Public License version 2 or later; see LICENSE.txt
+ */
+// No direct access
+defined('_JEXEC') or die;
+
+use \Joomla\CMS\Factory;
+use \Joomla\CMS\Language\Text;
+use \Joomla\CMS\MVC\View\HtmlView;
+use \Joomla\CMS\Uri\Uri;
+use \Joomla\CMS\HTML\HTMLHelper;
+
+/**
+ * View to edit
+ * @since  1.6
+ */
+class GavotingViewPositionform extends HtmlView
+{
+	protected $state;
+
+	protected $item;
+
+	protected $form;
+
+	protected $params;
+
+	protected $canSave;
+
+	/**
+	 * Display the view
+	 *
+	 * @param   string  $tpl  Template name
+	 *
+	 * @return void
+	 *
+	 * @throws Exception
+	 */
+	public function display($tpl = null)
+	{
+		$app  = Factory::getApplication();
+		$user = Factory::getUser();
+
+		$this->state   = $this->get('State');
+		$this->item    = $this->get('Item');
+		$this->params  = $app->getParams('com_gavoting');
+		$this->canSave = $this->get('CanSave');
+		$this->form		= $this->get('Form');
+
+		// Check for errors.
+		if (count($errors = $this->get('Errors')))
+		{
+			throw new Exception(implode("\n", $errors));
+		}
+
+		return parent::display($tpl);
+	}
+
+}
